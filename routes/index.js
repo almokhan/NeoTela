e65adad6a245779a628a1d235c79fd44b91f1c45
almokhan <khanankha@proton.me>
2024-10-08 14:21:09 -0300

    CSS, Variaveis e muito +

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // Carrega o arquivo index.handlebars
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Neo | Index' });
+  res.render('index', { title: 'NeoTela' });
 });
 
 module.exports = router;
