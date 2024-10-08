e0733ca896ba36685f9125e60bfdc0195f27809c
almokhan <khanankha@proton.me>
2024-10-08 15:58:29 -0300

    Ajustado conforme a tabela do cliente

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // Carrega o arquivo index.handlebars
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'NeoTela' });
+  res.render('index', { title: 'NeoTela', } );
 });
 
 module.exports = router;
